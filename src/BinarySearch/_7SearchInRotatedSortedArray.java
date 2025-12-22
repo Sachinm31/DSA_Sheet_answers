723f6e82da6ce39e5fbd7c5a2d3b422464fe7175
Sachinm31 <sachinm5657@gmail.com>
2025-12-22 18:30:41 +0530

    Leetcode 33 solution added

---
+++ src/BinarySearch/_7SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+public class _7SearchInRotatedSortedArray {
+
+    public static int search(int nums[],int target){
+        int n = nums.length;
+        int start = 0;
+        int end =n-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[mid]>=nums[start]){
+                if(target>=nums[start] && target<nums[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(nums[mid]<target && target<=nums[end]){
+                    start=mid+1;
+                }
+            }
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int nums[]={4,5,6,7,8,0,1,2,3};
+
+        System.out.println(search(nums,6));
+        System.out.println(search(nums,1));
+    }
+}
